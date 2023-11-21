bc5e85da0fe845d81526eb2e710f17769b8d5cc7
Sebastian Solarte <johan.solarte@kiwibot.com>
2023-11-21 17:13:12 +0000

    Eliminate RVIZ from the launch file

--- lslidar_driver/launch/lslidar_c32_launch.py
+++ lslidar_driver/launch/lslidar_c32_launch.py
@@ -2,22 +2,15 @@
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import LifecycleNode
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
 
-import lifecycle_msgs.msg
 import os
 import subprocess
 
 
 def generate_launch_description():
     driver_dir = os.path.join(get_package_share_directory('lslidar_driver'), 'params', 'lslidar_c32.yaml')
-    rviz_dir = os.path.join(get_package_share_directory('lslidar_driver'), 'rviz_cfg', 'lslidar_c32.rviz')
-
     p = subprocess.Popen("echo $ROS_DISTRO", stdout=subprocess.PIPE, shell=True)
     driver_node = ""
-    rviz_node = ""
     ros_version = p.communicate()[0]
     print(ros_version)
     if ros_version == b'dashing\n' or ros_version == b'eloquent\n':
@@ -29,13 +22,6 @@ def generate_launch_description():
                                     output='screen',
                                     parameters=[driver_dir],
                                     )
-        rviz_node = Node(
-            package='rviz2',
-            node_namespace='c32',
-            node_executable='rviz2',
-            node_name='rviz2',
-            arguments=['-d', rviz_dir],
-            output='screen')
     elif ros_version == b'foxy\n' or ros_version == b'galactic\n' or ros_version == b'humble\n' or ros_version == b'iron\n':
         print("ROS VERSION: foxy/galactic/humble")
         driver_node = LifecycleNode(package='lslidar_driver',
@@ -46,18 +32,10 @@ def generate_launch_description():
                                     emulate_tty=True,
                                     parameters=[driver_dir],
                                     )
-        rviz_node = Node(
-            package='rviz2',
-            namespace='c32',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_dir],
-            output='screen')
-
     else:
         print("Please configure the ros environment")
         exit()
 
     return LaunchDescription([
-        driver_node, rviz_node
+        driver_node
     ])
